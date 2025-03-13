a48488b8698982096533185d3e8443fc624c4262
Harry Keller <ich@harry-k.de>
2025-03-13 17:46:45 +0100

    [feature] english site (#19)

---
+++ translations/de/site.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+  "404!" => "404!",
+  "Blog" => "Blog",
+  "Continue reading" => "Weiterlesen",
+  "Home" => "Startseite",
+  "Links" => "Links",
+  "More articles" => "Mehr Artikel",
+  "No entries yet. Create some in the CMS!" => "Es gibt bisher keine Einträge.",
+  "Open Readme on GitHub" => "Readme auf Github",
+  "Our blog" => "Unser Blog",
+  "Our projects" => "Unsere Projekte",
+  "Page not found" => "Seite nicht gefunden",
+  "Project Status: {status}" => "Projekt-Status: {status}",
+  "Projects" => "Projekte",
+  "Return to homepage" => "Zurück zur Homepage",
+  "Round: {round}" => "Runde: {round}",
+  "Switch to dark mode" => "Zum dunklen Modus wechseln",
+  "Switch to light mode" => "Zum hellen Modus wechseln",
+  "Team Members" => "Team-Mitglieder",
+  "This is a pre-configured <a href='https://www.craftcms.com'>Craft CMS</a> installation, created by <a href='https://www.village.one'>Village One</a>. Please check out the Readme in the project root or on Github for first steps." => "Dies ist eine vorkonfigurierte <a href='https://www.craftcms.com'>Craft CMS</a>-Installation, erstellt von <a href='https://www.village.one'>Village One</a>. Eine kurze Einführung gibt es in der Readme-Datei im Dateisystem oder auf Github.",
+  "Welcome!" => "Willkommen!",
+];
